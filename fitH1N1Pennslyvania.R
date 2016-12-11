826e8bf6775f0c1fa9e4ab2d58cc880fb610e713
jstockwin <jstockwin@gmail.com>
2016-12-11 18:17:46 +0000

    Remove redundant files now that the server is working

--- fitH1N1Pennslyvania.R
+++
@@ -1,8 +0,0 @@
-source("main.R")
-
-serialIntervalData <-  read.csv("datasets/PennsylvaniaH1N12009SerialIntervalData.csv", header = FALSE)
-casesPerDayData <- read.csv("datasets/PennsylvaniaH1N12009FluData.csv", header = FALSE)
-
-
-transmissibility_estimation(serialIntervalData, casesPerDayData, 5)
-
