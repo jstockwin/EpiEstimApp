1c16a39f5b26bee3f73efff439be322125b86166
jstockwin <jstockwin@gmail.com>
2016-12-17 11:11:59 +0000

    Move processing functions into utils.R

--- server.R
+++ server.R
@@ -14,6 +14,7 @@ source("src/EstimateRnew.R", local=TRUE)
 source("src/DiscrSI.R", local=TRUE)
 source("src/OverallInfectivity.R", local=TRUE)
 source("src/stochasticSEIRModel3.R", local=TRUE)
+source("utils.R", local=TRUE)
 
 # By default, the file size limit is 5MB. It can be changed by
 # setting this option. Here we'll raise limit to 9MB.
@@ -34,17 +35,8 @@ shinyServer(function(input, output) {
                                    header = input$header, sep = input$sep,
                                    quote = input$quote)
     
-    num_cols = dim(serialIntervalData)[2]
-    if (num_cols < 4 || num_cols > 5) {
-      stop("serialIntervalData should have 4 or 5 columns")
-    } else if (num_cols == 4) {
-      # Add the type column manually
-      serialIntervalData[5] <- 0
-    }
-    # serialIntervalData will now have 5 columns.
-    names <- c("EL", "ER", "SL", "SR", "type")
-    colnames(serialIntervalData) <- names
-    serialIntervalData <- as.data.frame(serialIntervalData)
+    # Process the data (see function in utils.R)
+    serialIntervalData <- processSerialIntervalData(serialIntervalData)
     
     # Only use 80 host pairs' interval data to estimate the serial interval
     return(dic.fit.mcmc(dat = serialIntervalData, dist=input$SIDist))
@@ -90,31 +82,14 @@ shinyServer(function(input, output) {
       
       incProgress(1/2, detail = paste("Processing day data"))
       
-      
-      ## Pre-process the casesPerDayData
-      
-      cases_dims <- dim(casesPerDayData)
-      if ((cases_dims[1] == 1 && cases_dims[2] > 1) || (cases_dims[1] == 2 && cases_dims[2] > 2)) {
-        # The data is transposed.
-        casesPerDayData <- t(casesPerDayData)
-        # Update cases_dims for next bit
-        cases_dims <- dim(casesPerDayData)
-      }
-      
-      if (cases_dims[2] > 2) {
-        # Bad input
-        stop("casesPerDayData should only have one column, or one column and an index column")
-      } else if (cases_dims[2] == 1) {
-        # Add a time column first.
-        casesPerDayData <- cbind(seq.int(nrow(casesPerDayData)), casesPerDayData)
-      }
-      colnames(casesPerDayData) <- c("Time", "Cases")
-      casesPerDayData <- as.data.frame(casesPerDayData)
+      # Process data (see utils.R)
+      casesPerDayData <- processCasesPerDayData(casesPerDayData)
       
       ####  FEED INTO EPIESTIM
       W <- input$W
+      length <- dim(casesPerDayData)[1]
       incProgress(3/4, detail = paste("Plotting graphs..."))
-      EstimateR(casesPerDayData[,2], T.Start=1:(cases_dims[1] - W), T.End=(1+W):cases_dims[1], n2 = dim(fit@samples)[2], CDT = fit, plot=TRUE)
+      EstimateR(casesPerDayData[,2], T.Start=1:(length - W), T.End=(1+W):length, n2 = dim(fit@samples)[2], CDT = fit, plot=TRUE)
       incProgress(1/10, detail = paste("Done"))
     })
   })
---
+++ utils.R
@@ -0,0 +1,34 @@
+processSerialIntervalData <- function (serialIntervalData) {
+  print(dim(serialIntervalData))
+  num_cols = dim(serialIntervalData)[2]
+  if (num_cols < 4 || num_cols > 5) {
+    stop("serialIntervalData should have 4 or 5 columns")
+  } else if (num_cols == 4) {
+    # Add the type column manually
+    serialIntervalData[5] <- 0
+  }
+  # serialIntervalData will now have 5 columns.
+  names <- c("EL", "ER", "SL", "SR", "type")
+  colnames(serialIntervalData) <- names
+  serialIntervalData <- as.data.frame(serialIntervalData)
+}
+
+processCasesPerDayData <- function (casesPerDayData) {
+  cases_dims <- dim(casesPerDayData)
+  if ((cases_dims[1] == 1 && cases_dims[2] > 1) || (cases_dims[1] == 2 && cases_dims[2] > 2)) {
+    # The data is transposed.
+    casesPerDayData <- t(casesPerDayData)
+    # Update cases_dims for next bit
+    cases_dims <- dim(casesPerDayData)
+  }
+  
+  if (cases_dims[2] > 2) {
+    # Bad input
+    stop("casesPerDayData should only have one column, or one column and an index column")
+  } else if (cases_dims[2] == 1) {
+    # Add a time column first.
+    casesPerDayData <- cbind(seq.int(nrow(casesPerDayData)), casesPerDayData)
+  }
+  colnames(casesPerDayData) <- c("Time", "Cases")
+  casesPerDayData <- as.data.frame(casesPerDayData)
+}
