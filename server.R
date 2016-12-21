65d8aea3c85dad15458368c952fb6ae16396771f
jstockwin <jstockwin@gmail.com>
2016-12-21 09:27:56 +0000

    Tell client we're done even on error so JS resets buttons

--- server.R
+++ server.R
@@ -30,64 +30,73 @@ shinyServer(function(input, output, session) {
   output$plot <- renderPlot({
     input$go
     input$mydata
-    if (!is.null(input$mydata) && input$mydata == "STOP") return()
-    isolate({
-      # This if-else handles the special case of uploaded data slightly differently.
-      if (input$data == 'Uploaded Data'){
-        # Read the data
-        serialIntervalDataFile <- input$serialIntervalData
-        casesPerDayDataFile <- input$casesPerDayData
-        
-        if (is.null(serialIntervalDataFile)) {
-          return(NULL)
+    # Put everything in a dry catch, as we need to send the "DONE" message to the client
+    # even if we exit with an error.
+    tryCatch({
+      if (!is.null(input$mydata) && input$mydata == "STOP") return()
+      isolate({
+        # This if-else handles the special case of uploaded data slightly differently.
+        if (input$data == 'Uploaded Data'){
+          # Read the data
+          serialIntervalDataFile <- input$serialIntervalData
+          casesPerDayDataFile <- input$casesPerDayData
+          
+          if (is.null(serialIntervalDataFile)) {
+            return(NULL)
+          }
+          
+          # Runs the reactive function defined below which will take a long time if the uploaded data
+          # is new or has changed, but will otherwise immediately complete allowing the thread to move on quickly
+          # if the data has not changed (helpful when e.g. changing the width, W, only).
+          samples = get_uploaded_samples()
+          
+          if (dim(samples)[1] < 8000) {
+            # We are not done. Check if client wants to stop
+            data <- toJSON(samples)
+            session$sendCustomMessage(type='pingToClient', data) 
+            return()
+          }
+          
+          samples <- samples[3000:8000,] #Remove burnin
+          
+          if (is.null(casesPerDayDataFile)) {
+            return(NULL)
+          }
+          
+          # Load the casesPerDay data
+          casesPerDayData <- read.csv(casesPerDayDataFile$datapath, 
+                                      header = input$header, sep = input$sep,
+                                      quote = input$quote)
+          # Process casesPerDay data (see utils.R)
+          casesPerDayData <- processCasesPerDayData(casesPerDayData)
+        } else {
+          # Load the data
+          casesPerDayData <- getCasesPerDayData(input$data)
+          serialIntervalData <- getSerialIntervalData(input$data)
+          
+          # If the distribution is set to offset gamma, we should check this is reasonable.
+          if (input$SIDist == 'off1G' && any(serialIntervalData[4] - serialIntervalData[1] < 1)) {
+            stop('The chosen dataset has serial intervals which are definitely less than 1,
+             so a gamma distribution offset by 1 is not appropriate.')
+          }
+          # Get the MCMCFit (see utils.R)
+          samples <- getMCMCFit(input$data, input$SIDist)@samples
+          
         }
         
-        # Runs the reactive function defined below which will take a long time if the uploaded data
-        # is new or has changed, but will otherwise immediately complete allowing the thread to move on quickly
-        # if the data has not changed (helpful when e.g. changing the width, W, only).
-        samples = get_uploaded_samples()
         
-        if (dim(samples)[1] < 8000) {
-          # We are not done. Check if client wants to stop
-          data <- toJSON(samples)
-          session$sendCustomMessage(type='pingToClient', data) 
-          return()
-        }
-        
-        samples <- samples[3000:8000,] #Remove burnin
-        
-        if (is.null(casesPerDayDataFile)) {
-          return(NULL)
-        }
-        
-        # Load the casesPerDay data
-        casesPerDayData <- read.csv(casesPerDayDataFile$datapath, 
-                                    header = input$header, sep = input$sep,
-                                    quote = input$quote)
-        # Process casesPerDay data (see utils.R)
-        casesPerDayData <- processCasesPerDayData(casesPerDayData)
-      } else {
-        # Load the data
-        casesPerDayData <- getCasesPerDayData(input$data)
-        serialIntervalData <- getSerialIntervalData(input$data)
-        
-        # If the distribution is set to offset gamma, we should check this is reasonable.
-        if (input$SIDist == 'off1G' && any(serialIntervalData[4] - serialIntervalData[1] < 1)) {
-          stop('The chosen dataset has serial intervals which are definitely less than 1,
-           so a gamma distribution offset by 1 is not appropriate.')
-        }
-        # Get the MCMCFit (see utils.R)
-        samples <- getMCMCFit(input$data, input$SIDist)@samples
-        
-      }
-      
-      
-      ####  FEED INTO EPIESTIM
-      W <- input$W
-      length <- dim(casesPerDayData)[1]
-      EstimateR(casesPerDayData[,2], T.Start=1:(length - W), T.End=(1+W):length, n2 = dim(samples)[2], CDT = samples, dist=input$SIDist, plot=TRUE)
+        ####  FEED INTO EPIESTIM
+        W <- input$W
+        length <- dim(casesPerDayData)[1]
+        EstimateR(casesPerDayData[,2], T.Start=1:(length - W), T.End=(1+W):length, n2 = dim(samples)[2], CDT = samples, dist=input$SIDist, plot=TRUE)
+        session$sendCustomMessage(type='done', "")
+      }) # End Isolate
+    },
+    error = function (e) {
+      # Send message to client that we're done.
       session$sendCustomMessage(type='done', "")
-    }) # End Isolate
+      stop(e)
+    }) # End tryCatch
     
   }) # End output$plot
   
