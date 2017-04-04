1c1c524c9ed25517d09b52f8cf3a47a9e69534b2
jstockwin <jstockwin@gmail.com>
2017-04-04 15:37:57 +0100

    All states working

--- server.R
+++ server.R
@@ -31,6 +31,14 @@ alldatasets <- list('Measles1861' = Measles1861,
                     'Flu2009' = Flu2009)
 
 
+# Part of workaround below ref: https://github.com/HenrikBengtsson/future/issues/137
+# The below functions aren't in the workspace at all. We'll add them like this so
+# they can then be mentioned in the future to make them globals, so they can be used asyncronously.
+process_SI.Data <- EpiEstim:::process_SI.Data
+check_SI.Distr <- EpiEstim:::check_SI.Distr
+### end part of workaround
+
+
 # Source necessary files
 source("dic.fit.mcmc.incremental.R", local=TRUE)
 source("stochasticSEIRModel3.R", local=TRUE)
@@ -43,7 +51,7 @@ options(shiny.maxRequestSize = 9*1024^2)
 options(shiny.reactlog=TRUE) 
 
 allStates = c("1.1", "2.1", "2.2", "3.1", "4.1", "5.1", "6.1", "6.2", "7.1", "7.2", "7.3", "7.4",
-             "8.1", "8.2", "8.3", "8.4", "8.5", "9.1", "9.2", "9.3")
+              "8.1", "8.2", "8.3", "8.4", "8.5", "9.1", "9.2", "9.3")
 
 finalStates = c("8.1", "9.1", "8.3", "7.3", "8.4", "9.2", "9.3")
 
@@ -83,8 +91,10 @@ shinyServer(function(input, output, session) {
   
   # Clicking previous/next should increment the stateLevel
   observeEvent(input$nxt, {
+    values$status = "Processing"
     if (handleState()) {
       values$state = getNextState(values$state)
+      values$status = "Ready"
     }
   })
   observeEvent(input$prev, {
@@ -108,32 +118,44 @@ shinyServer(function(input, output, session) {
   
   # Logic for when "go" is clicked.
   observeEvent(input$go, {
-    #tryCatch({
+    values$status = "Running..."
+    asyncData$epiEstimOutput <- NULL # Remove current plot
+    tryCatch({
       if (handleState()) {
-        values$status = "Running"
         startAsyncDataLoad("epiEstimOutput", future({
-          EstimateR_func # Workaround ref: https://github.com/HenrikBengtsson/future/issues/137
+          ##########################
+          # # Workaround ref: https://github.com/HenrikBengtsson/future/issues/137
+          # # (Make certain things globals)
+          EstimateR_func 
+          OverallInfectivity
+          process_SI.Data 
+          .Random.seed
+          dic.fit.mcmc
+          check_SI.Distr
+          # # End workaround
+          ##########################
           EstimateR(IncidenceData, T.Start, T.End, method=method, n1=n1, n2=n2, Mean.SI = Mean.SI, Std.SI = Std.SI, 
-                                                              Std.Mean.SI = Std.Mean.SI, Min.Mean.SI = Min.Mean.SI, Max.Mean.SI = Max.Mean.SI, Std.Std.SI = Std.Std.SI,
-                                                              Min.Std.SI = Min.Std.SI, Max.Std.SI = Max.Std.SI, SI.Distr = SI.Distr, SI.Data = SI.Data, 
-                                                              SI.parametricDistr = SI.parametricDistr, MCMC.control = MCMC.control, SI.Sample = SI.Sample, plot = plot)
-          }))
+                    Std.Mean.SI = Std.Mean.SI, Min.Mean.SI = Min.Mean.SI, Max.Mean.SI = Max.Mean.SI, Std.Std.SI = Std.Std.SI,
+                    Min.Std.SI = Min.Std.SI, Max.Std.SI = Max.Std.SI, SI.Distr = SI.Distr, SI.Data = SI.Data, 
+                    SI.parametricDistr = SI.parametricDistr, MCMC.control = MCMC.control, SI.Sample = SI.Sample, plot = plot)
+        }))
       }
-    #},
-    #error = function (e) {
-    #  ## TODO: Graciously handle as many expected errors as possible. 
-    #  ## Perhaps make a new function handleErrors(error, state)?
-    #  info(e$message) ## TODO: This is just a JS alert for now. Should be done better.
-    #  error(e)
-    #})
+    },
+    error = function (e) {
+      ## TODO: Graciously handle as many expected errors as possible. 
+      ## Perhaps make a new function handleErrors(error, state)?
+      info(e$message) ## TODO: This is just a JS alert for now. Should be done better.
+      stop(e) # Debug only!
+    })
   })
   
   output$plot <- renderPlot({
-    if (!is.null(asyncData[["epiEstimOutput"]])) {
-      p_I <- plots(asyncData[["epiEstimOutput"]], what="I")
-      #p_SI <- plots(values$epiEstimOutput, what="SI")
-      p_R <- plots(asyncData[["epiEstimOutput"]], what="R")
+    if (!is.null(asyncData$epiEstimOutput)) {
+      p_I <- plots(asyncData$epiEstimOutput, what="I")
+      #p_SI <- plots(asyncData$epiEstimOutput, what="SI")
+      p_R <- plots(asyncData$epiEstimOutput, what="R")
       gridExtra::grid.arrange(p_I,p_R,ncol=1)
+      values$status <- "Ready"
     }
   })
   
@@ -144,13 +166,13 @@ shinyServer(function(input, output, session) {
     # and should set all necessary variables.
     # The state will change only if handleState returns TRUE. 
     tryCatch({
-    switch(state,
-           "1.1" = {TRUE},
-           "2.1" = {
+      switch(state,
+             "1.1" = {TRUE},
+             "2.1" = {
                # Handle uploaded data:
                IncidenceData <<- read.csv(input$incidenceData$datapath, 
-                                         header = input$incidenceHeader, sep = input$incidenceSep,
-                                         quote = input$incidenceQuote)
+                                          header = input$incidenceHeader, sep = input$incidenceSep,
+                                          quote = input$incidenceQuote)
                # Process Incidence data (see utils.R)
                IncidenceData <<- processIncidenceData(IncidenceData)
                
@@ -159,29 +181,105 @@ shinyServer(function(input, output, session) {
                T.Start <<- 1:(length - W)
                T.End <<- (1+W):length
                TRUE
-           },
-           "2.2" = {
-             # Get preloaded data
-             IncidenceData <<- getIncidenceData(input$incidenceDataset, alldatasets)
-             
-             length <- dim(IncidenceData)[1]
-             W <- input$uploadedWidth
-             T.Start <<- 1:(length - W)
-             T.End <<- (1+W):length
-             TRUE
-           },
-           "5.1" = {TRUE},
-           "6.1" = {TRUE},
-           "7.1" = {TRUE},
-           "8.1" = {
-             # "SIFromSample"
-             # Simply read the MCMC samples from the file. See getMCMCFit in utils.R 
-             method <<- "SIFromSample"
-             SI.Sample <<- getSISamples(input$SIDataset, input$SIDist)
-             TRUE
-           },
-           stop(sprintf("An error occurred in handleState(). Input '%s' was not recognised.", state))
-     )
+             },
+             "2.2" = {
+               # Get preloaded data
+               IncidenceData <<- getIncidenceData(input$incidenceDataset, alldatasets)
+               
+               # Process Incidence data (see utils.R)
+               IncidenceData <<- processIncidenceData(IncidenceData)
+               
+               length <- dim(IncidenceData)[1]
+               W <- input$uploadedWidth
+               T.Start <<- 1:(length - W)
+               T.End <<- (1+W):length
+               TRUE
+             },
+             "3.1" = {TRUE},
+             "4.1" = {
+               ImportedData <- read.csv(input$importedData$datapath,
+                                        header = input$importedHeader, sep = input$importedSep,
+                                        quote = input$importedQuote)
+               # Process Incidence data (see utils.R) (IncidenceData will have been handled in state 2.1)
+               IncidenceData <- processIncidenceData(IncidenceData, ImportedData)
+               TRUE
+             },
+             "5.1" = {TRUE},
+             "6.1" = {TRUE},
+             "6.2" = {TRUE},
+             "7.1" = {TRUE},
+             "7.2" = {TRUE},
+             "7.3" = {
+               method <<- "UncertainSI"
+               n1 <<- input$n1
+               n2 <<- input$n2
+               Mean.SI <<- input$Mean.SI
+               Std.SI <<- input$Std.SI
+               Std.Mean.SI <<- input$Std.Mean.SI
+               Min.Mean.SI <<- input$Min.Mean.SI
+               Max.Mean.SI <<- input$Max.Mean.SI
+               Std.Std.SI <<- input$Std.Std.SI
+               Min.Std.SI <<- input$Min.Std.SI
+               Max.Std.SI <<- input$Max.Std.SI
+               TRUE
+             },
+             "7.4" = {TRUE},
+             "8.1" = {
+               # "SIFromSample"
+               # Simply read the MCMC samples from the file. See getMCMCFit in utils.R 
+               method <<- "SIFromSample"
+               SI.Sample <<- getSISamples(input$SIDataset, input$SIDist)
+               TRUE
+             },
+             "8.2" = {
+               method <<- "SIFromData"
+               serialIntervalData <- read.csv(input$SIData$datapath, 
+                                              header = input$SIHeader, sep = input$SISep,
+                                              quote = input$SIQuote)
+               # Process the data (see function in utils.R)
+               SI.Data <<- processSerialIntervalData(serialIntervalData)
+               TRUE
+             },
+             "8.3" = {
+               SI.Sample <<- processSISamples(read.csv(input$SISampleData$datapath, 
+                                                       header = input$SISampleHeader, sep = input$SISampleSep,
+                                                       quote = input$SISampleQuote))
+               n2 <<- input$n23
+               TRUE
+             },
+             "8.4" = {
+               Mean.SI <<- input$Mean.SI2
+               Std.SI <<- input$Std.SI2
+               method <<- "ParametricSI"
+               TRUE
+             },
+             "8.5" = {TRUE},
+             "9.1" = {
+               MCMC.control$burnin <<- input$burnin
+               MCMC.control$thin <<- input$thin
+               if (!is.na(input$param1) && !is.na(input$param1)) {
+                 MCMC.control$init.pars <<- c(input$param1, input$param2)
+               }
+               n1 <<- input$n12
+               n2 <<- input$n22
+               SI.parametricDistr <<- input$SIDist2
+               MCMC.control$seed <<- 1 # TODO: REMOVE THIS! There is an issue with EpiEstim's default behaviour right now.
+               TRUE
+             },
+             "9.2" = {
+               method <<- "NonParametricSI"
+               SI.Distr <<- read.csv(input$SIDistrData$datapath, 
+                                     header = input$SIDistrHeader, sep = input$SIDistrSep,
+                                     quote = input$SIDistrQuote)
+               TRUE
+             },
+             "9.3" = {
+               method <<- "NonParametricSI"
+               SI.Distr <<- alldatasets[[input$SIDistrDataset]]$SI.Distr
+               TRUE
+             },
+             stop(sprintf("An error occurred in handleState(). Input '%s' was not recognised.", state))
+      )
     },
     error = function (e) {
       ## TODO: Graciously handle as many expected errors as possible. 
@@ -199,7 +297,7 @@ shinyServer(function(input, output, session) {
            "1.1" = {if (input$incidenceDataType == "own") "2.1" else "2.2"},     
            "2.1" = {"3.1"},
            "2.2" = {"5.1"},
-           "3.1" = {"4.1"},
+           "3.1" = {if (input$imported == "TRUE") "4.1" else "5.1"},
            "4.1" = {"5.1"},
            "5.1" = {if (input$SIPatientData == "TRUE") "6.1" else "6.2"},
            "6.1" = {if (input$SIDataType == "preloaded") "7.1" else "7.2"},
@@ -256,8 +354,20 @@ shinyServer(function(input, output, session) {
     for (asyncDataName in names(asyncDataBeingLoaded)) {
       asyncFutureObject <- asyncDataBeingLoaded[[asyncDataName]]
       if (resolved(asyncFutureObject)) {
-        asyncData[[asyncDataName]] <<- value(asyncFutureObject)
-        asyncDataBeingLoaded[[asyncDataName]] <<- NULL
+        tryCatch({
+          asyncData[[asyncDataName]] <<- value(asyncFutureObject)
+          asyncDataBeingLoaded[[asyncDataName]] <<- NULL
+        },
+        error = function (e) {
+          # Note any errors in anything called asyncronously (most important stuff!) will error here, not
+          # when the async process is started.
+          ## TODO: Graciously handle as many expected errors as possible. 
+          ## Perhaps make a new function handleErrors(error, state)?
+          checkAsyncDataBeingLoaded$suspend() # Stop running, otherwise we'll throw the error every 1000ms.
+          info(e$message) ## TODO: This is just a JS alert for now. Should be done better.
+          #stop(e) # Debug only!
+          FALSE
+        })
       }
     }#end loop over async data items being loaded
     #if there are no more asynchronous data items being loaded then stop checking
