f6c89da35476006d449a2eb120e60d522584709e
jstockwin <jstockwin@gmail.com>
2017-04-17 00:08:00 +0100

    Updates

--- tests/test-connection.r
+++ tests/test-connection.r
@@ -8,23 +8,22 @@ user <- Sys.getenv("SAUCE_USERNAME") # Your Sauce Labs username
 pass <- Sys.getenv("SAUCE_ACCESS_KEY") # Your Sauce Labs access key 
 port <- 4445 
 ip <- paste0(user, ':', pass, "@localhost")
-rdBrowser <- "chrome"
-version <- "33"
-platform <- "OS X 10.9"
 extraCapabilities <- list(name = "RSelenium OS/Browsers vignette first example", username = user
                           , accessKey = pass, tags = list("RSelenium-vignette", "OS/Browsers-vignette"))
 
 
-remDr <- remoteDriver$new(remoteServerAddr = ip, port = port, browserName = rdBrowser
-                          , version = version, platform = platform
-                          , extraCapabilities = extraCapabilities)
+remDr <- remoteDriver$new(remoteServerAddr = ip, port = port, extraCapabilities = extraCapabilities)
 remDr$open()
-Sys.sleep(120)
 appUrl="http://localhost:6000"
-test_that("can connect to app", {
-	remDr$navigate(appUrl)
-	appTitle <- remDr$getTitle()[[1]]
-	expect_equal(appTitle, "http://localhost:6000/")
+tryCatch({
+	test_that("can connect to app", {
+		remDr$navigate(appUrl)
+		appTitle <- remDr$getTitle()[[1]]
+		expect_equal(appTitle, "http://localhost:6000/")
+	})
+},
+error = function(e) {
+	remDr$close()
+	stop(e)
 })
-
 remDr$close()
