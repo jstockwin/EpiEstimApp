aa9a16b9e2a96dc8f0dcfb5bd1bbeacd0a77f879
jstockwin <jstockwin@gmail.com>
2017-04-17 01:08:52 +0100

    Use travis sauce connect

--- tests/test-connection.r
+++ tests/test-connection.r
@@ -9,7 +9,8 @@ pass <- Sys.getenv("SAUCE_ACCESS_KEY") # Your Sauce Labs access key
 port <- 4445 
 ip <- paste0(user, ':', pass, "@localhost")
 extraCapabilities <- list(name = "RSelenium OS/Browsers vignette first example", username = user
-                          , accessKey = pass, tags = list("RSelenium-vignette", "OS/Browsers-vignette"))
+                          , accessKey = pass, tags = list("RSelenium-vignette", "OS/Browsers-vignette")
+			  , startConnect = FALSE, tunnelIdentifier = Sys.getenv("TRAVIS_JOB_NUMBER"))
 
 
 remDr <- remoteDriver$new(remoteServerAddr = ip, port = port, extraCapabilities = extraCapabilities)
@@ -18,7 +19,7 @@ appUrl="http://localhost:3000"
 tryCatch({
 	test_that("can connect to app", {
 		remDr$navigate(appUrl)
-		titleElem <- remDr$findElements("id", "incidenceTitle")
+		titleElem <- remDr$findElement(using="id", "incidenceTitle")
 		title <- titleElem$getElementText()
 		expect_equal(title, "Incidence Data")
 	})
