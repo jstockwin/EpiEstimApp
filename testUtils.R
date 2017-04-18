19e3ae7ac1ea9f22b2d2d095e705955528983af4
jstockwin <jstockwin@gmail.com>
2017-04-18 15:47:02 +0100

    Fix previous commit

--- testUtils.R
+++ testUtils.R
@@ -31,3 +31,43 @@ runTests <- function (browsers="all", platforms="all", update=FALSE) {
     out
   }
 }
+
+getRemoteDriver <- function(name, browserName, platform) {		
+  # Set's up sauce connect on travis, or if the		
+  # sauceUsername and sauceAccessKey are set in R		
+  # Otherwise attempts to connect to a local selenium server on		
+  # localhost:4444. Make sure you're running the app on		
+  # port 3000 in a different process: `R -e "shiny::runApp(port=3000)`.		
+  sauceLabs <- TRUE		
+  if (!exists("sauceUsername")) {		
+    if (Sys.getenv("SAUCE_USERNAME") != "") {		
+      user <- Sys.getenv("SAUCE_USERNAME") # Your Sauce Labs username		
+    } else {		
+      sauceLabs <- FALSE		
+    }		
+  } else {		
+    user <- sauceUsername		
+  }		
+  
+  if (!exists("sauceAccessKey")) {		
+    if (Sys.getenv("SAUCE_ACCESS_KEY") != "") {		
+      pass <- Sys.getenv("SAUCE_ACCESS_KEY") # Your Sauce Labs access key		
+    } else {		
+      sauceLabs <- FALSE		
+    }		
+  } else {		
+    pass <- sauceAccessKey		
+  }		
+  
+  if (sauceLabs) {		
+    port <- 4445 		
+    ip <- paste0(user, ':', pass, "@localhost")		
+    extraCapabilities <- list(name = name, username = user, accessKey = pass		
+                              , startConnect = FALSE, tunnelIdentifier = Sys.getenv("TRAVIS_JOB_NUMBER"))		
+    remDr <- remoteDriver$new(remoteServerAddr = ip, port = port, extraCapabilities = extraCapabilities		
+                              , browserName = browserName, platform = platform)		
+  } else {		
+    remDr <- remoteDriver$new(remoteServerAddr = "localhost", port = 4444, browserName = browserName)		
+  }		
+  return(remDr)		
+} 
