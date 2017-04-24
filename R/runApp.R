13e27d3edbb89ded25f0da89d3706f5e18a977b0
Jake Stockwin <jstockwin@gmail.com>
2017-04-24 19:03:13 +0100

    Update code example

--- R/runApp.R
+++ R/runApp.R
@@ -2,10 +2,13 @@
 #'
 #' This function runs EpiEstimApp
 #' @param port The port to run on. Defaults to 3000.
+#' @param ... Additional arguments
 #' @keywords run
 #' @export
 #' @examples
+#' \dontrun{
 #' runEpiEstimApp()
+#' }
 
 runEpiEstimApp <- function(port=3000, ...) {
   appDir <- system.file("app", package="EpiEstimApp")
