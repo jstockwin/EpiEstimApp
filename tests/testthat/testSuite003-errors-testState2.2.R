55307406464a7c4518fee962f771d8a43752267d
Jake Stockwin <jstockwin@gmail.com>
2017-07-11 11:32:02 +0100

    -[tests] Added validation testing for state 2.2

---
+++ tests/testthat/testSuite003-errors-testState2.2.R
@@ -0,0 +1,50 @@
+context("Test Suite 3 (Errors) --> State 2.2")
+
+library(RSelenium)
+library(testthat)
+source("functions.R", local=TRUE)
+
+drivers <- getRemDrivers("Test Suite 3 (Errors) --> State 2.2")
+rD <- drivers$rDr
+remDr <- drivers$remDr
+
+openRemDriver(remDr)
+tryCatch({
+  test_that("can connect to app", {
+    connectToApp(remDr)
+  })
+
+  test_that("app is ready within 30 seconds", {
+    waitForAppReady(remDr)
+  })
+
+  test_that("can navigate to state 2.2", {
+   navigateToState(remDr, "2.2")
+  })
+
+  test_that("Giving an invalid prior mean throws correct error", {
+    clear(remDr, pages$state2.2$selectors$meanPriorInput)
+    sendKeys(remDr, pages$state2.2$selectors$meanPriorInput, "-1")
+    clickNext(remDr)
+    checkError(remDr, "Prior mean must be non-negative", "incidenceMeanPrior")
+    # Reset for upcoming tests
+    clear(remDr, pages$state2.2$selectors$meanPriorInput)
+    sendKeys(remDr, pages$state2.2$selectors$meanPriorInput, "5")
+  })
+
+  test_that("Giving an invalid prior sd throws correct error", {
+    clear(remDr, pages$state2.2$selectors$stdPriorInput)
+    sendKeys(remDr, pages$state2.2$selectors$stdPriorInput, "0")
+    clickNext(remDr)
+    checkError(remDr, "Prior standard deviation must be positive", "incidenceStdPrior")
+    # Reset for upcoming tests
+    clear(remDr, pages$state2.2$selectors$stdPriorInput)
+    sendKeys(remDr, pages$state2.2$selectors$stdPriorInput, "5")
+  })
+},
+error = function(e) {
+  closeRemDrivers(remDr, rD)
+  stop(e)
+})
+
+closeRemDrivers(remDr, rD)
