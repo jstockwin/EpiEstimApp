a9fe5981bf79fd87678a3826602a5176e492fd84
Robin Thompson <robin.thompson@lmh.ox.ac.uk>
2018-03-03 14:26:55 +0000

    Change description of ideas in the body text of state 7.1

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -184,8 +184,8 @@ shiny::shinyUI(shiny::bootstrapPage(theme = shinythemes::shinytheme("spacelab"),
          )),
          shinyjs::hidden(div(id = "7.1",
                     # State 7.1
-                    p("Please note that these preloaded data are only to give an idea as to how the app works. As such, the full range of options are not provided here (e.g. the number of steps in the MCMC chain is fixed)."), #nolint (can't split html)
-                    HTML("<p>If you would like to have the full range of options, please download the serial interval data for these datasets from <a href='https://github.com/jstockwin/EpiEstimApp/tree/master/inst/app/datasets/SerialIntervalData' target='_blank'>here</a> then go back a step and select 'Own data' then 'Raw exposure data'.</p>"), #nolint can't split html
+                    p("Please note that these preloaded datasets are only to provide examples as to how the app might be used. As such, the full range of options are not provided here (e.g. the number of steps in the MCMC chain is fixed)."), #nolint (can't split html)
+                    HTML("<p>If you would like to have the full range of options, please download the serial interval data for these datasets from <a href='https://github.com/jstockwin/EpiEstimApp/tree/master/inst/app/datasets/SerialIntervalData' target='_blank'>here</a>, then go back a step and select 'Own data' followed by 'Raw exposure data'.</p>"), #nolint can't split html
                     div(id = "si_dataset_error_box", class = "error_box",
                         shiny::radioButtons("si_dataset", "Choose your dataset",
                                    c("RotavirusEcuador2011", "H1N1NewYork2009",
